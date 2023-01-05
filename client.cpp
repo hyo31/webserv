10a5a59ffecb6df580bcfb5db22abded99c61a59
Milan andrei daniel Groen <mgroen@f1r3s3.codam.nl>
2023-01-05 16:24:37 +0100

    stress test

--- client.cpp
+++ client.cpp
@@ -6,44 +6,52 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <iostream>
+#include <time.h>
 
 #define PORT 8002
 
 int main(int argc, char const *argv[])
 {
-    int sock = 0; long valread;
+    if (argc != 2)
+        return 0;
+    int sock = 0;
     struct sockaddr_in serv_addr;
     // char hello[500] = "POST /uploadForm.pl HTTP/1.1\r\nHost: localhost:8004\r\nUser-Agent: Go-http-client/1.1\r\nTransfer-Encoding: chunked\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept-Encoding: gzip\r\n\r\n20\r\nfname=bass&lname=nop&message=hoi\r\n0\r\n\r\n";
-	char hello[500] = "DELETE /pages/abc.txt HTTP/1.1\r\nHost: localhost\r\n\r\n";
+	char hello[500] = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+    time_t start, now;
+    float runTime = std::stof(argv[1]), elapsedTime = 0;
 
+    time(&start);
+    while (elapsedTime < runTime)
+    {
+        char buffer[6000] = {0};
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            printf("\n Socket creation error \n");
+            return -1;
+        }
 
-    char buffer[6000] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-    
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    send(sock, hello, sizeof(hello) , 0);
-    printf("Hello message sent\n");
-	ssize_t kek = recv(sock, buffer, 6000, 0);
+        memset(&serv_addr, '0', sizeof(serv_addr));
 
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+
+        // Convert IPv4 and IPv6 addresses from text to binary form
+        if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+        {
+            printf("\nInvalid address/ Address not supported \n");
+            return -1;
+        }
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+            printf("\nConnection Failed \n");
+            return -1;
+        }   
+        send(sock, hello, sizeof(hello) , 0);
+        printf("Hello message sent\n");
+	    recv(sock, buffer, 6000, 0);
+        now = time(NULL);
+        elapsedTime = difftime(now, start);
+    }
     return 0;
 }
